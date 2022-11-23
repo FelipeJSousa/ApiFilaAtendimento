7d515a488d27560cb1a24534d1fb4c6c3b67f0ec
Felipe Sousa <csfelipe8@gmail.com>
2022-11-22 21:26:15 -0300

    feat(Fila): add Get Fila + Get Fila/:id #1

--- src/controllers/fila/fila.py
+++ src/controllers/fila/fila.py
@@ -1,18 +1,29 @@
 from fastapi import APIRouter
 
 from src.schemas.fila import CriarFila
+from src.schemas.fila import Fila
 
 fila_router = APIRouter()
+db_fila = []
 
+def obter_fila_nao_atendidos():
+    _fila = [x for x in db_fila if x.atendido == False]
+    return _fila;
 
 @fila_router.get("/fila")
-async def obter_fila():
-    return {"filas": []}
+async def get_fila():
+    _ret = obter_fila_nao_atendidos();
+    if(len(_ret) > 0):
+        return {"filas": _ret}
+    return {}
 
 
 @fila_router.get("/fila/{_id}")
 async def obter_fila_id(_id: int):
     ...
+async def get_fila_id(_id: str):
+    _fila = [x for x in obter_fila_nao_atendidos() if x.id == _id]
+    return {"fila": _fila}
 
 
 @fila_router.post("/fila")
